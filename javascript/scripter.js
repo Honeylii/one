ca68efa635d48df9f9e9b8cc91f16f1e813055f4
Honeylii <oriadeoxy@gmail.com>
2022-01-19 10:09:52 +0100

    Second commit on the mario club site

--- javascript/scripter.js
+++ javascript/scripter.js
@@ -1,11 +1,26 @@
-function makeUp(){
-    const bid = 22;
-    let counter = 0;
-    if(bid){
-        counter ++;
-        console.log("The counnter is updating....")
-        setInterval(e =>{
-            counter ++;
-        },200)
+// function makeUp(){
+//     const bid = prompt("Enter a true or false");
+//     let counter = 0;
+//     if(bid){
+//         counter ++;
+//         setInterval(e =>{
+//             console.log("The counnter is updating.... " + counter )
+//             alert(" Counter Loading " + counter ++);
+//         },200)
+//     }
+// }
+
+// makeUp();
+
+function isName(){
+    var isCounter = prompt("Enter your name");
+    while(isCounter == ""){
+        var counter = 0;
+        alert("counter loading... " + counter++)
+        console.log("counter laoding.... " +counter++)
+
+        isCounter = prompt("Enter your name")
     }
 }
+
+isName();
